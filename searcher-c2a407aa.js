0e3bdd15c5f5799209257f6995a305f2c7311838
dylanbstorey <dylanbstorey@users.noreply.github.com>
2026-03-14 11:54:24 +0000

    deploy: mimir-dm/mimir@88960016d77839746156fc1652f5e0d8140c8a54

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-905ad687.js',
+                path_to_root + 'searchindex-771f5ecf.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
